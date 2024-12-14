763835571bfb0ab987bc997b589787699bc076da
aysel <aysel.vural@outlook.com>
2024-12-14 21:24:45 +0100

    conflict check 3

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,4 +1,4 @@
 def write_test_about_branch():
     print("making some changes")
-    print("hey it is me, main branch!")
+    print("hey it is me, main branch!!!")
     print("change on main branch to see the conflict again!")
