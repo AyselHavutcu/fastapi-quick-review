3872da1ba528081a2f53a68b3c2c7e5ffb19c793
aysel <aysel.vural@outlook.com>
2024-12-14 23:40:21 +0100

    conflict resolved

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -3,3 +3,4 @@ def write_test_about_branch():
     print("hey it is me, main branch!!!")
     print("change on main branch to see the conflict again!")
     print("Last Conflict check with rebase !!!")
+    print("change 3 to test_branch")
