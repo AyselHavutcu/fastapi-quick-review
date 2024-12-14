e89cd923998a9c89fe8db36591122dd95c51997c
aysel <aysel.vural@outlook.com>
2024-12-14 23:36:56 +0100

    main conflict check

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -2,3 +2,4 @@ def write_test_about_branch():
     print("making some changes")
     print("hey it is me, main branch!!!")
     print("change on main branch to see the conflict again!")
+    print("Last Conflict check with rebase !!!")
