187878ac2820f3f7e5a83b031d28d37858882b88
aysel <aysel.vural@outlook.com>
2024-12-14 20:50:22 +0100

    commit 1 to main

--- app/main.py
+++ app/main.py
@@ -21,6 +21,6 @@ app.include_router(auth.router)
 
 @app.get("/")
 async def root():
-    print("added some things to main branch")
+    print("changed my main branch")
     return {"message" : "Hello World!!!! here is a change to see the rebase"}
 
